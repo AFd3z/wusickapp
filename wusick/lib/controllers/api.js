c8cd9ff983c38c53d06b2673fab9bf7119767cff
jha86 <jha@Planet01-PC>
2014-05-28 12:22:37 +0200

    registro

--- wusick/lib/controllers/api.js
+++ wusick/lib/controllers/api.js
@@ -79,11 +79,9 @@ exports.obtieneTipoUsuario= function (req,res,callback){
 //check if mail exists
 exports.existeMail = function (req,res) {
 
-	
-	
 	var mail = req.body.email;
 	var json ="";
-	//var mail = req.body.email;
+	
     //creating connection object
     var sqlconnection = connection.createConnection();
     var query = 'SELECT * FROM usuarios WHERE email="'+mail+'"';
@@ -109,37 +107,47 @@ exports.registro = function (req,res) {
 	
 		var user = req.body.nombre;
 		var pass = req.body.pass;
-		var email = req.body.mail
-		
-		
+		var email = req.body.email;
+		var tipo = req.body.tipo;
 		
 		var json ="";
 
 		    //creating connection object
 		    var sqlconnection = connection.createConnection();
-		    var query = 'INSERT INTO USUARIOS (nombre, password,email,fecha_alta,Tipo_usuarios_idTipo_usuarios)VALUES ';
+		    var query = 'INSERT INTO USUARIOS (nombre, password,email,fecha_alta,Tipo_usuarios_idTipo_usuarios) VALUES ("'+user+'","'+pass+'","'+email+'",curdate(),'+tipo+') ';
 
 		    //sending query through our connection object
 		    sqlconnection.query(query, function(err, results,fields) {
 		        if (err)
-		            return callback(err, "query error");
-
-		       if (results.length<=0){
-
-		        console.log("login incorrecto"+results);
-		        res.send('null');
-
-		       }else{
-
-		        console.log("login correcto del usuario: "+user);
-		        json = JSON.stringify(results);
+		            res.send(err, "query error");
+		        
+		        var insertedID=results.insertId;
+		        console.log("registro correcto del usuario: "+user+" con ID: "+results.insertId);
+		        
+		        //inserting specific data into specific user type table
+		        switch (tipo){
+		        case 1:
+		        	var query = 'INSERT INTO basicos (fecha_nac,sexo, Usuarios_idUsuario) VALUES ("'+req.body.fecha+'","'+req.body.sexo+'","'+insertedID+'") ';
+		        	break;
+		        case 2:
+		        	var query = 'INSERT INTO artistas (Genero, Usuarios_idUsuario) VALUES ("'+req.body.genero+'","'+req.body.sexo+'","'+insertedID+'") ';
+		        	break;
+		        case 3:
+		        	var query = 'INSERT INTO salas (aforo,poblacion,direccion, Usuarios_idUsuario) VALUES ("'+req.body.fecha+'","'+req.body.sexo+'","'+insertedID+'") ';
+		        	break;	
+		        
+		        
+		        
+		        
+		        
+		        }
+		        
 		        //returning jsonized result
 		        //res.json(json);
 		        //returning json object
 		        res.json(results);
 
-		       }
-
+		       
 		        sqlconnection.end();
 
 		    });
