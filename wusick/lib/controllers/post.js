76a1ea7a264b9421f5259807b29391de094ae151
Javi <jha1986@gmail.com>
2014-06-03 21:17:47 +0200

    faltaba una comilla en la sentencia

--- wusick/lib/controllers/post.js
+++ wusick/lib/controllers/post.js
@@ -52,7 +52,7 @@ exports.feed = function (req,res) {
 	var id =req.params.id;
 
 	var sqlconnection = connection.createConnection();
-	var query = 'select tr.Usuarios_idUsuario logueado, tp.idPosts idPost, tp.contenido contenido, tp.fecha fecha, tp.post_img img, tp.destinatario destinatario, tp.Usuarios_idUsuario autor from usuarios_has_usuarios tr, posts tp where tr.Usuarios_idUsuario = '+id+' and (tp.Usuarios_idUsuario = tr.Usuarios_idUsuario1 or tp.Usuarios_idUsuario = '+id+') order by tp.fecha desc;
+	var query = 'select tr.Usuarios_idUsuario logueado, tp.idPosts idPost, tp.contenido contenido, tp.fecha fecha, tp.post_img img, tp.destinatario destinatario, tp.Usuarios_idUsuario autor from usuarios_has_usuarios tr, posts tp where tr.Usuarios_idUsuario = '+id+' and (tp.Usuarios_idUsuario = tr.Usuarios_idUsuario1 or tp.Usuarios_idUsuario = '+id+') order by tp.fecha desc';
 	           
 	    sqlconnection.query(query, function(err, results) {
 	            if (err){
